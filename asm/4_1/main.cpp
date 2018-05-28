a263c428c2d4cb46f0ef8f69608ee5393cc0c9c4
Denis <daletskidenis@gmail.com>
2018-05-28 23:54:37 +0300

    asm done

--- asm/4_1/main.cpp
+++ asm/4_1/main.cpp
@@ -151,15 +151,53 @@ std::pair<bool, std::vector<char>> palindrome(const std::vector<char>& digits) {
 
 
 extern "C" {
-//	int __fastcall can_be_palindrome(int x, int n);
-	int __cdecl can_be_palindrome2(int x, int y);
+	char* __fastcall can_be_palindrome(const char* seq, int n);
 }
+
+
+void checkSequence(const std::vector<char>& seq) {
+	std::cout << "Input sequence: ";
+	for (int i = 0; i < seq.size(); ++i) {
+		std::cout << int(seq[i]) << " ";
+	}
+	std::cout << std::endl;
+
+	if (!seq.size()) {
+		std::cout << "empty sequence" << std::endl << std::endl;
+		return;
+	}
+
+	char* result = can_be_palindrome(seq.data(), seq.size());
+	if (result == 0) {
+		std::cout << "impossible" << std::endl;
+	}
+	else {
+		std::cout << "possible: ";
+		for (int i = 0; i < seq.size(); ++i) {
+			std::cout << int(result[i]);
+		}
+	}
+
+	std::cout << std::endl << std::endl;
+}
+
+
 
 int main(int argc, char* argv[]) {
 	std::vector<char> seq;
-	seq = { 2, 1, 3, 3, 0, 0, 0, 1, 2 };
 
-	std::cout << can_be_palindrome2(10, 23);
+	seq = { 2, 1, 3, 3, 0, 0, 0, 1, 2 };
+	checkSequence(seq);
+
+	seq = { 2, 1, 3, 3, 0, 0, 0, 0, 1, 2 };
+	checkSequence(seq);
+
+
+	seq = {1, 1, 1, 3, 3, 3};
+	checkSequence(seq);
+
+	seq = {1};
+	checkSequence(seq);
 
 	system("pause");
 	return 0;
--- asm/4_1/palindrome.asm
+++ asm/4_1/palindrome.asm
@@ -1,21 +1,136 @@
-PUBLIC @can_be_palindrome@8
+.486
+.model flat
 
-.686
-.MODEL FLAT, C
+.data
+freq db 10 dup(0)
+size_ dword 0
+dst db 1000 dup(0)
 
-.STACK
-.DATA
-.CODE
-
-
-@can_be_palindrome@8 PROC
-mov eax, ecx;
-add eax, edx;
-ret;
-@can_be_palindrome@8 ENDP
-
-_can_be_palindrome2 PROC
-_can_be_palindrome2 ENDP
+.code
 
 
+@can_be_palindrome@8 proc
+	; ecx is sequence
+	; edx is seq size
+
+	push ebx;
+	push esi;
+
+	mov size_, edx;
+	mov esi, edx;
+	add esi, ecx;  // esi points to the end of src
+;	mov edx, freq;
+
+	loop_start:
+		cmp ecx, esi;
+		jge loop_end;
+
+		xor eax, eax;
+		mov al, [ecx];
+		xor ebx, ebx;
+		mov bl, freq[eax];
+		inc bl;
+		mov freq[eax], bl;
+
+		inc ecx;
+		jmp loop_start;
+
+	loop_end:
+		mov ecx, 0;     left index
+		mov esi, size_
+		dec esi ;       right index
+		mov ebx, 0;     ebx is current freq's index
+
+	before:
+		cmp esi, ecx;
+		jl pre_end;
+
+		xor eax, eax;
+		mov al, freq[ebx];
+		cmp al, 2;
+		jl next_dig
+
+		;// add current digit to dst ///
+		mov dst[ecx], bl;
+		mov dst[esi], bl;
+		;///////////////////////////////
+
+		dec esi;
+		inc ecx;
+
+		cmp ebx, 10;
+		jge pre_end;
+
+		xor eax, eax;
+		mov al, freq[ebx];
+		sub al, 2;
+		mov freq[ebx], al;
+
+		jmp before;
+
+	next_dig:
+		inc ebx;
+		jmp before;
+
+	pre_end:
+		mov ebx, 0;      // ebx is current freq's index
+
+		xor eax, eax;  // eax is sum of remained freqs
+
+	sstart:
+		mov al, freq[ebx];
+		add ah, al;
+
+		cmp al, 1;
+		je mark_digit;
+	after_mark:
+
+		inc ebx;
+
+		cmp ebx, 10;
+		jge eend;
+
+		jmp sstart;
+
+	eend:
+		cmp ah, 1;
+		jl done;
+		jg impossible;
+		
+		mov eax, size_;
+		xor edx, edx;
+
+		push ecx;
+		mov ecx, 2
+		div ecx;
+		pop ecx;
+
+		mov ebx, esi;
+		mov dst[eax], bl;
+
+		jmp done;
+
+	mark_digit:
+		mov esi, ebx;
+		jmp after_mark;
+
+
+	impossible:
+		mov eax, 0;
+		jmp very_end;
+
+	done:
+		mov eax, offset dst;
+		jmp very_end;
+
+	very_end:
+		pop esi;
+		pop ebx;
+
+	ret
+@can_be_palindrome@8 endp
+
+
+;----------------------------------------------
 END
+;----------------------------------------------
