766c746af0fab730dd507f2884113c90fc91d210
Denis Daletski <daletskidenis@gmail.com>
2018-09-08 01:59:52 +0300

    ndarray temp

--- computational/ndarray/main.cpp
+++ computational/ndarray/main.cpp
@@ -1,14 +1,38 @@
 #include <ndarray.h>
 #include <iostream>
 
+template<template<class, class> class SeqType, class ItemType, class Alloc=std::allocator<ItemType>>
+std::ostream& operator << (std::ostream& str, const SeqType<ItemType, Alloc>& seq) {
+    str << "[ ";
+    for(auto obj : seq) {
+        str << obj << " ";
+    }
+    str << "]";
+    return str;
+}
 
 int main() {
 	std::cout << std::endl;
-    NDArray<int> arr({2, 3, 4}, 2);
+    NDArray<int> arr({2, 3, 4});
     std::cout << arr << std::endl;
     std::cout << arr.count() << std::endl;
 
-    arr({1, 1, 0}) = 1;
+    arr({0, 0, 0}) = 1;
+
     std::cout << arr << std::endl;
+
+    NDArray<int> copy = arr;
+
+    std::cout << "copy: " << copy << std::endl;
+    std::cout << "orig: " << arr << std::endl;
+
+    NDArray<int> moved = std::move(arr);
+
+    std::cout << "moved: " << moved << std::endl;
+
+    std::cout << moved.shape() << std::endl;
+    std::cout << arr.shape() << std::endl;
+
+    std::cout << moved.max(-1) << std::endl;
     return 0;
 }
--- computational/ndarray/ndarray.h
+++ computational/ndarray/ndarray.h
@@ -2,9 +2,12 @@
 #include <vector>
 #include <numeric>
 #include <functional>
+#include <algorithm>
 #include <iostream>
+#include <cstring>
+#include <memory>
 
-typedef size_t Dim;
+typedef int Dim;
 typedef std::vector<Dim> Index;
 typedef Index Shape;
 typedef Index TailSlice;
@@ -14,8 +17,8 @@ template <class DataType>
 class NDArray {
 private:
     Shape _shape;
-    Shape _shapeCumProd;
-    DataType* _data;
+    Shape _strides;
+    std::shared_ptr<DataType[]> _data;
 
     bool checkInBounds(const Index& index) {
         for(int i = 0; i < index.size(); ++i) {
@@ -25,24 +28,24 @@ private:
         return true;
     }
 
-    void calculateShapeProd() {
-        _shapeCumProd.assign(_shape.begin(), _shape.end());
-        _shapeCumProd.push_back(1);
+    void calculateStrides() {
+        _strides.assign(_shape.begin(), _shape.end());
+        _strides.push_back(1);
 
-        for(int i = _shapeCumProd.size() - 2; i >= 0; --i) {
-            _shapeCumProd[i] *= _shapeCumProd[i+1];
+        for(int i = _strides.size() - 2; i >= 0; --i) {
+            _strides[i] *= _strides[i+1];
         }
     }
 
     size_t indexToPos(const Index& index) const {
         size_t pos = 0;
         for(int i = 0; i < index.size(); ++i) {
-            pos += index[i] * _shapeCumProd[i+1];
+            pos += index[i] * _strides[i+1];
         }
         return pos;
     }
 
-    std::pair<size_t, size_t> sliceBounds(const TailSlice& sliceCoords) const {
+    std::pair<size_t, size_t> tailSliceBounds(const TailSlice& sliceCoords) const {
         auto index = sliceCoords;
 
         index.push_back(0);
@@ -58,7 +61,7 @@ private:
         size_t curr_dim = curr_slice.size();
 
         if(curr_dim == _shape.size() - 1) {
-            std::pair<size_t, size_t> bounds = sliceBounds(curr_slice);
+            std::pair<size_t, size_t> bounds = tailSliceBounds(curr_slice);
             for(int i = bounds.first; i < bounds.second; ++i) {
                 ss << _data[i];
                 if(i < bounds.second-1) {
@@ -79,24 +82,58 @@ private:
     }
 
 public:
-    NDArray(const Index shape, const DataType & value)
-        : _shape(shape)
-    {
-        calculateShapeProd();
+    NDArray(const Index shape) : _shape(shape) {
+        calculateStrides();
         size_t full_size = count();
 
-        _data = new DataType[full_size];
+        _data.reset(new DataType[full_size]);
+    }
+
+    NDArray(const Index shape, const DataType & value) : NDArray(shape) {
+        size_t full_size = count();
         for(int i = 0; i < full_size; ++i) {
             _data[i] = value;
         }
     }
 
     ~NDArray() {
-        delete[] _data;
     }
 
+
+    NDArray(const NDArray<DataType>& other) : _data(nullptr) {
+        *this = other;
+    }
+
+    NDArray(NDArray<DataType>&& other) : _data(nullptr) {
+        *this = std::move(other);
+    }
+
+    NDArray<DataType>& operator = (const NDArray<DataType>& other) {
+        _shape = other._shape;
+        calculateStrides();
+
+        _data = other._data;
+
+        return *this;
+    }
+
+    NDArray<DataType>& operator = (NDArray<DataType>&& other) {
+        _shape = std::move(other._shape);
+        _strides = std::move(other._strides);
+
+        _data = other._data;
+
+        return *this;
+    }
+
+    ////////////// getters ////////////////////
+
     size_t count() const {
-        return _shapeCumProd.front();
+        return _strides.front();
+    }
+
+    const Shape& shape() const {
+        return _shape;
     }
 
     const DataType& operator() (const Index & index) const {
@@ -118,6 +155,63 @@ public:
         }
         return _data[indexToPos(index)];
     }
+
+    /////////////////// shape and layout methods //////////////////////
+
+    void reshape(const Shape & shape) {
+        size_t newTotalSize = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<size_t>());
+        if(count() != newTotalSize) {
+            throw "incompatible shapes";
+        }
+
+        _shape = shape;
+        calculateStrides();
+    }
+
+    NDArray<DataType> reshaped(const Shape & shape) {
+        NDArray<DataType> copy = *this;
+        copy.reshape(shape);
+        return copy;
+    }
+
+    void transposed(const std::vector<size_t>& dims_permutation) {
+        Shape trShape;
+        Shape trStrides;
+
+        // permute strides (starting from second) and shape according to permutation
+
+        trStrides.push_back(_strides.front());
+        for(auto dim : dims_permutation) {
+            trStrides.push_back(_strides[dim+1]);
+            trShape.push_back(_shape[dim]);
+        }
+        // indexToPos will point to data so as it's transposed
+
+        // change shape to permuted
+
+        // reshape data (mb per-element copy using indexToPos)
+    }
+
+
+    ///////////////////// reducers ////////////////////////
+
+    const DataType& max(const Dim& dim) {
+        if(dim < 0) {
+            return *std::max_element(_data.get(), _data.get() + count(), std::less<DataType>());
+        } else {
+            return DataType();
+        }
+    }
+
+    const DataType& min(const Dim& dim) {
+        if(dim < 0) {
+            return *std::min_element(_data.get(), _data.get() + count(), std::less<DataType>());
+        } else {
+            return DataType();
+        }
+    }
+
+    ///////////////////////////////////////////////////////
 
     friend std::ostream& operator << (std::ostream& stream, const NDArray & array) {
         stream << "[";
