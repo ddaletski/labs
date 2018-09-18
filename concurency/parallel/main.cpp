25e4ad43ee5aa83e204dd15c52aa4f2973def4d9
Denis Daletski <DaletskiDenis@gmail.com>
2018-09-18 14:20:55 +0300

    accumulate fix

--- concurency/parallel/main.cpp
+++ concurency/parallel/main.cpp
@@ -8,11 +8,13 @@
 
 template<class Func, class ... Args>
 double timeit(Func f, Args ... args) {
-    double start = clock();
+    using hrc = std::chrono::high_resolution_clock;
 
+    auto start = hrc::now();
     f(args...);
+    auto end = hrc::now();
 
-    return 1.0 * (clock() - start) / CLOCKS_PER_SEC;
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 }
 
 
@@ -38,12 +40,15 @@ T accumulate_parallel(Iterator first, Iterator last, const T& init, BinaryOp op)
     Iterator block_end = first;
     std::vector<T> results(threads_count);
 
+    auto result_it = results.begin();
+
     for(size_t i = 0; i < threads_count-1; ++i) {
         std::advance(block_end, block_size);
-        threads.emplace_back(accumulate_sequential<Iterator, T, BinaryOp>, std::ref(results[i]), block_start, block_end, init, op);
+        threads.emplace_back(accumulate_sequential<Iterator, T, BinaryOp>, std::ref(*result_it), block_start, block_end, init, op);
         block_start = block_end;
+        ++result_it;
     }
-    threads.emplace_back(accumulate_sequential<Iterator, T, BinaryOp>, std::ref(results.back()), block_start, last, init, op);
+    threads.emplace_back(accumulate_sequential<Iterator, T, BinaryOp>, std::ref(*result_it), block_start, last, init, op);
 
     for(auto it = threads.begin(); it != threads.end(); ++it) {
         it->join();
@@ -55,27 +60,32 @@ T accumulate_parallel(Iterator first, Iterator last, const T& init, BinaryOp op)
     return result;
 }
 
+typedef double MyType;
+
+MyType f(MyType x, MyType y) {
+    return pow(pow(x, 5) + pow(y, 5), 1.0 / 6);
+}
+
 
 int main() {
-    for(int elems_count = 1e3; elems_count <= 1e9; elems_count *= 10) {
+    for(MyType elems_count = 1e3; elems_count <= 1e8; elems_count *= 10) {
         std::cout << "elems count: " << elems_count << std::endl;
 
-        std::vector<int> vec(elems_count);
-        for(int i = 0; i < elems_count; ++i) {
+        std::vector<MyType> vec(elems_count);
+        for(MyType i = 0; i < elems_count; ++i) {
             vec[i] = rand() % 100;
         }
 
-        int result = 1;
+        MyType result = 1;
+        MyType initial_value = 1;
 
-        accumulate_sequential(result, vec.begin(), vec.end(), 0, std::plus<int>());
-        int parallel_result = accumulate_parallel(vec.begin(), vec.end(), 0, std::plus<int>());
-
-        std::cout << result << " | " << parallel_result << std::endl;
-
-        double spent_seq = timeit(accumulate_sequential<decltype(vec.begin()), int, std::function<int(int, int)>>, result, vec.begin(), vec.end(), 0, std::plus<int>());
-        double spent_parallel = timeit(accumulate_parallel<decltype(vec.begin()), int, std::function<int(int, int)>>, vec.begin(), vec.end(), 0, std::plus<int>());
+        double spent_seq = timeit(accumulate_sequential<decltype(vec.begin()), MyType, std::function<MyType(MyType, MyType)>>, result, vec.begin(), vec.end(), initial_value, f);
+        std::cout << "---\n";
+        double spent_parallel = timeit(accumulate_parallel<decltype(vec.begin()), MyType, std::function<MyType(MyType, MyType)>>, vec.begin(), vec.end(), initial_value, f);
 
         std::cout << "(" << spent_seq << " | " << spent_parallel << ")" << std::endl;
+
+        std::cout << std::endl;
     }
 
     return 0;
