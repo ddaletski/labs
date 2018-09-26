38835dc056e63baa5d5e8f573cd3237338253b94
Denis Daletski <DaletskiDenis@gmail.com>
2018-09-26 19:25:33 +0300

    java lab 4

--- java/lab4_1/src/Main.java
+++ java/lab4_1/src/Main.java
@@ -14,17 +14,32 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BoolVector vec = randomVector(10);
+        System.out.println("vector1:");
         System.out.println(vec);
         BoolVector vec2 = randomVector(10);
+        System.out.println("vector2:");
         System.out.println(vec2);
 
-        System.out.println(vec.length());
-        System.out.println(vec.countOnes());
-        System.out.println(vec.countZeros());
+        System.out.printf("vector1:: length: %d, zeros: %d, ones: %d\n", vec.length(), vec.countZeros(), vec.countOnes());
+        System.out.println("vector1 inversed:");
         System.out.println(vec.inversed());
 
+        System.out.println("vector1 & vector2:");
         System.out.println(vec2.conjunction(vec));
+        System.out.println("vector1 | vector2:");
         System.out.println(vec2.disjunction(vec));
 
+
+        try {
+            randomVector(6).conjunction(randomVector(8));
+        } catch (Exception ex) {
+            System.out.printf("exception caught as expected: %s\n", ex.getMessage());
+        }
+
+        try {
+            randomVector(6).disjunction(randomVector(8));
+        } catch (Exception ex) {
+            System.out.printf("exception caught as expected: %s\n", ex.getMessage());
+        }
     }
 }
