0c399a9458a12c9b96f587dbcdf5a9b1bbe7b37b
Denis Daletski <daletskidenis@gmail.com>
2018-12-09 12:53:00 +0300

    comp lab 3

--- computational/lab3/inverse_power_iteration.py
+++ computational/lab3/inverse_power_iteration.py
@@ -1,25 +1,37 @@
 import numpy as np
 
-def min_eig_std(A: np.matrix):
-    return min(np.linalg.eigvals(A), key=np.abs)
+def min_max_eig_std(A: np.matrix):
+    eigvals = np.linalg.eigvals(A)
+    return min(eigvals, key=np.abs), max(eigvals, key=np.abs)
 
-def min_eig(A: np.matrix):
+
+def power_iteration(A: np.matrix):
     n = A.shape[0]
     b = np.zeros(n)
     b[0] = 1
-    A_inv = np.linalg.inv(A)
-
-    steps = 0
+    
     diff = b
     while np.linalg.norm(diff, ord=np.inf) > 1e-12:
         prev = b
-        b = (A_inv @ b) / np.linalg.norm(b, ord=np.inf)
+        b = (A @ b) / np.linalg.norm(b, ord=np.inf)
         diff = np.abs(prev - b)
-        steps += 1
+        yield b
 
+
+def min_eig(A: np.matrix):
+    A_inv = np.linalg.inv(A)
+    bs = list(power_iteration(A_inv))
+    b = bs[-1]
     eigenval = b.T @ A @ b / (b.T @ b)
-    return steps, eigenval
 
+    return len(bs), eigenval
+
+def max_eig(A: np.matrix):
+    bs = list(power_iteration(A))
+    b = bs[-1]
+    eigenval = b.T @ A @ b / (b.T @ b)
+
+    return len(bs), eigenval
 
 if __name__ == "__main__":
     np.set_printoptions(floatmode="fixed", precision=16)
@@ -32,8 +44,11 @@ if __name__ == "__main__":
         ], dtype=float)
 
 
-    std_eig = min_eig_std(A)
-    steps, calculated_val = min_eig(A)
+    std_min_eig, std_max_eig = min_max_eig_std(A)
+    min_steps, _min_eig = min_eig(A)
+    max_steps, _max_eig = max_eig(A)
 
-    print(f"std: {std_eig}")
-    print(f"found: {calculated_val} in {steps} steps")
+    print(f"min: {std_min_eig}")
+    print(f"max: {std_max_eig}")
+    print(f"found min: {_min_eig} in {min_steps} steps")
+    print(f"found max: {_max_eig} in {max_steps} steps")
